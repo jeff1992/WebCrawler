6356925b4f2ba194d835e30cb53f31fbbaf13383
jeff.wang <jeff.wang@wesoft.com>
2019-05-29 20:22:41 +0800

    update node

--- src/nodejs/sample.js
+++ src/nodejs/sample.js
@@ -1,20 +1,28 @@
 var axios = require('axios');
 var cheerio = require('cheerio');
 
-
 axios.request({
   url: 'https://news.sina.com.cn/china/'
 }).then(res => {
   // 通过 load 方法把 HTML 代码转换成一个 jQuery 对象
   var $ = cheerio.load(res.data);
+  var urls = [];
   $('a').each((i, e) => {
-    var title = $(e).text();
     var href = $(e).attr('href');
-    if(href && href.match(/https:\/\/news.sina.com.cn\//)) {  //筛选出指向新闻的链接
-      console.log(`title:${title}  href:${href}`);
-      //接下来进行存储...
+    if(href && href.match(/https:\/\/news.sina.com.cn\//)) {
+      urls.push(href);
     }
+  });
+  return urls.splice(0, 10);  //演示，象征性地爬一下就好了
+}).then(urls => {
+  //爬详情
+  urls.forEach(url => {
+    axios.request(url).then(res => {
+      var $ = cheerio.load(res.data);
+      var title = $('h1.main-title').text();
+      var date = $('div.date-source span.date').text();
+      var from = $('div.date-source a.source').text()
+      console.log(title, date, from);
+    })
   })
-}).catch(res => {
-  console.log(res);
 })
